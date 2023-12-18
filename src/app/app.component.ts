b69d0a1c8ece35925fe9e63ad8ae7bf426576b2d
diegohyenna <dgmartins87@hotmail.com>
2023-12-18 08:08:38 -0300

    melhorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Home', url: '/folder/inbox', icon: 'home' },
-    // { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    // { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    // { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    // { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  ];
+  public appPages = [{ title: 'Home', url: '/pages/home', icon: 'home' }];
+
   constructor() {}
 }
